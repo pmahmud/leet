788e142c96fb1b89ae195dd8aa3b66f03c886c4e
Pallab Mahmud <pmahmud@users.noreply.github.com>
2019-10-30 15:36:52 -0700

    Create 42TrappingRain.java

---
+++ src/42TrappingRain.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        int water = 0;
+        int li = 0;
+        int ri = height.length - 1;
+        int lmax = 0;
+        int rmax = 0;
+        
+        while(li < ri) {
+            lmax = Math.max(lmax, height[li]);   
+            rmax = Math.max(rmax, height[ri]);
+            if (lmax < rmax) {
+                water += lmax - height[li];
+                li++;
+            } else {
+                water += rmax - height[ri];
+                ri--;                
+            } 
+        }
+        
+        return water;
+    }
+}
+
+// Trapping rain problem with the stack: https://www.youtube.com/watch?v=lhzrp3Nbj-w
